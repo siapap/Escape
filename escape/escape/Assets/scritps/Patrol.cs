0f4a3782e474e3b0fc57f509ce01768fbaafe9b9
Selena C <se12na@knights.ucf.edu>
2020-02-17 07:37:45 -0500

    Adjusted patrol code
    fixed move spots to empty game object and made it so patrol route was random instead of fixed

--- escape/escape/Assets/scritps/Patrol.cs
+++ escape/escape/Assets/scritps/Patrol.cs
@@ -6,31 +6,38 @@ public class Patrol : MonoBehaviour
 {
     public float speed;
 
-    public Transform[] moveSpots;
-    //array for all spots AI can move to
-    private int randomSpot;
-    //will pick random spot from array to spawn
+    public Transform moveSpots;
+    
+    
     private float waitTime;
     public float startWaitTime;
+    public float minX;
+    public float maxX;
+    public float minZ;
+    public float maxZ; 
+    //variables hold coordinates for patrol area
+    //Z was set as Y
 
     // Start is called before the first frame update
     void Start()
     {
         waitTime = startWaitTime;
-        randomSpot = Random.Range(0, moveSpots.Length);
+        moveSpots.position = new Vector2(Random.Range(minX, maxX), Random.Range(minZ, maxZ));
+        //guard moves to random positions throughout the scene
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector2.MoveTowards(transform.position, moveSpots[randomSpot].position, speed * Time.deltaTime);
-        //moves enemy to a random movespot location
+        transform.position = Vector2.MoveTowards(transform.position, moveSpots.position, speed * Time.deltaTime);
+        
 
-        if (Vector2.Distance(transform.position, moveSpots[randomSpot].position) < 0.2f)
+        if (Vector2.Distance(transform.position, moveSpots.position) < 0.2f)
         {
             if (waitTime <= 0)
             {
-                randomSpot = Random.Range(0, moveSpots.Length);
+                moveSpots.position = new Vector2(Random.Range(minX, maxX), Random.Range(minZ, maxZ));
                 waitTime = startWaitTime;
             }
             else
