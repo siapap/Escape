269eea7861521b34837fbf2aef5b19cd1050d4ad
Josh Rain <joshrain@knights.ucf.edu>
2020-02-17 00:29:02 -0500

    door animation

---
+++ escape/escape/Assets/scritps/automaticDoor.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class automaticDoor : MonoBehaviour
+{
+    private Animator door;
+
+    
+    void Start()
+    {
+        door = GetComponent<Animator>();
+
+    }
+
+  
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        door.SetTrigger("OpenDoor");
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        door.enabled = true;
+    }
+    void pauseAnimationEvent()
+    {
+        door.enabled = false;
+    }
+}
