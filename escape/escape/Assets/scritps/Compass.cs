86c46819ec79ab2cbfe74ab9325d3de7dc0140c4
Josh Rain <joshrain@knights.ucf.edu>
2020-02-19 12:26:54 -0500

    josh went crazy
    bear with me

---
+++ escape/escape/Assets/scritps/Compass.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Compass : MonoBehaviour
+{
+    public Vector3 NorthDirection;
+    public Transform Player;
+    public Quaternion GoalDirection;
+
+    public RectTransform NorthLayer;
+    public RectTransform GoalLayer;
+
+    public Transform goalLocation;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        ChangeNorthDirection();
+        ChangeGoalDirection();
+    }
+
+    public void ChangeNorthDirection()
+    {
+        NorthDirection.z = Player.eulerAngles.y;
+        NorthLayer.localEulerAngles = NorthDirection;
+    }
+
+    public void ChangeGoalDirection()
+    {
+        Vector3 dir = transform.position = goalLocation.position;
+
+        GoalDirection = Quaternion.LookRotation(dir);
+
+        GoalDirection.z = GoalDirection.y;
+        GoalDirection.x = 0;
+        GoalDirection.y = 0;
+
+        GoalLayer.localRotation = GoalDirection * Quaternion.Euler(NorthDirection);
+    }
+}
---
+++ escape/escape/Assets/scritps/CountDownTimer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountDownTimer : MonoBehaviour
+{
+    float currentTime = 0f;
+    float startingTime = 60f;
+
+    [SerializeField] Text countDownText;
+    
+    void Start()
+    {
+        currentTime = startingTime;
+    }
+
+    void Update()
+    {
+        currentTime -= 1 * Time.deltaTime;
+        countDownText.text = currentTime.ToString("0s");
+
+        if(currentTime <= 10)
+        {
+            //countDownText.color = Color;
+
+
+            if (currentTime <= 0)
+            {
+                currentTime = 0;
+            }
+        }
+        
+    }
+
+}
