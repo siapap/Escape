556c6d8715a36b79e1e0dce5bd1a94c8b41473d8
Selena C <se12na@knights.ucf.edu>
2020-02-26 11:50:36 -0500

    Test

---
+++ escape/escape/Assets/scritps/LOSEnemy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LOSEnemy : MonoBehaviour
+{
+    public float viewRadius;
+    public float viewAngle;
+    
+    public Vector3 DirFromAngle(float angleInDegrees)
+    {
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
